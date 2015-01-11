5803de480ba378b4e14a8d539945e834c67f4ef9
Brendan Nee <brendan@blinktag.com>
2015-01-11 03:35:12 -0800

    Expenses routes

--- routes/api.js
+++ routes/api.js
@@ -1,23 +1,65 @@
 var nconf = require('nconf'),
+    _ = require('underscore'),
     request = require('request'),
+    async = require('async'),
     db = require('../libs/database'),
     venmoApiUrl = 'https://api.venmo.com',
     bmwApiUrl = 'http://data.api.hackthedrive.com';
 
 
 exports.createExpense = function(req, res, next) {
-  db.createExpense({
-    user_id: req.session.user_id,
-    expense_id: 'id'
-  }, function(e) {
-    if(e) return next(e);
-    res.json(data);
+  var tripId = req.body.tripId,
+      note = req.body.note,
+      friends = JSON.parse(req.body.friends || '[]'),
+      costPerPerson = req.body.costPerPerson;
+
+  async.map(friends, function(friend, cb) {
+    var expense = {
+      amount: costPerPerson,
+      note: note
+    };
+
+    if(friend.id) {
+      expense.user_id = friend.id
+    } else if(friend.display_name.indexOf('@') !== -1) {
+      //send as email
+      expense.email = friend.display_name;
+    } else {
+      //send as phone number
+      expense.phone = friend.display_name;
+    }
+
+    postExpense(expense, cb);
+  }, function(e, results) {
+    if(e) return next(e)
+
+    db.createExpense({
+      user_id: req.session.bmw_user_id,
+      trip_id: tripId,
+      friends: friends,
+      costPerPerson : costPerPerson
+    }, function(e) {
+      if(e) return next(e);
+      res.json(results);
+    });
   });
+
+
+  function postExpense(expense, cb) {
+    request.post({
+      uri: venmoApiUrl + '/v1/payments',
+      qs: _.extend(expense, {access_token: req.session.venmo_access_token}),
+      json: true,
+      timeout: 10000
+    }, function(e, r, body) {
+      cb(e, body);
+    });
+  }
 };
 
 
 exports.getExpenses = function(req, res, next) {
-  db.getExpenses(req.session.user_id, function(e, expenses) {
+  db.getExpenses(req.session.bmw_user_id, function(e, expenses) {
     if(e) return next(e);
     res.json(expenses);
   });
