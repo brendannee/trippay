3288f6ff3f2ce759a29932e88c9adb5bf03602c9
Brendan Nee <brendan@blinktag.com>
2015-01-10 18:22:26 -0800

    oauth frontend

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 exports.index = function(req, res, next) {
-  if(req.session && req.session.access_token) {
-    res.render('trips', {loggedIn: true});
+  if(req.session && req.session.bmw_access_token && req.session.venmo_access_token) {
+    res.render('trips');
+  } else if(req.session && req.session.bmw_access_token ) {
+    res.render('index', {bmw: true});
   } else {
     res.render('index');
   }
@@ -8,7 +10,7 @@ exports.index = function(req, res, next) {
 
 
 exports.authenticate = function(req, res, next) {
-  if(!req.session || !req.session.access_token) {
+  if(!req.session || !req.session.bmw_access_token) {
     if(req.xhr) {
       var error = new Error('Not logged in')
       error.status = 401;
@@ -33,11 +35,14 @@ exports.force_https = function(req, res, next) {
 
 exports.check_dev_token = function(req, res, next) {
   // Allows local dev environent to specify access token
-  if(process.env.TOKEN) {
-    req.session.access_token = process.env.TOKEN;
+  if(process.env.BMW_TOKEN) {
+    req.session.bmw_access_token = process.env.BMW_TOKEN;
   }
-  if(process.env.USER_ID) {
-    req.session.user_id = process.env.USER_ID;
+  if(process.env.VENMO_TOKEN) {
+    req.session.venmo_access_token = process.env.VENMO_TOKEN;
+  }
+  if(process.env.VENMO_USER_ID) {
+    req.session.venmo_user_id = process.env.VENMO_USER_ID;
   }
   next();
 };
